3bb4b43edb745f3aa86e0968f74bfd24a55aa7c0
Vlad1m1ru5 <kubantsev.vov@mail.ru>
2022-03-25 15:53:58 +0300

    Liquidjs template markdown

--- pages/api/files/index.ts
+++ pages/api/files/index.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { readFile } from "node:fs/promises";
 import { STATUS_CODES } from "node:http";
 import incomingForm from "~/configs/incoming-form";
+import liquidProcessor from "~/configs/liquid-processor";
 import remarkProcessor from "~/configs/remark-processor";
 import supabaseAdmin from "~/configs/supabase-admin";
 
@@ -24,16 +25,18 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       vFile.data.name = vFile.data.name || files.file.originalFilename;
       vFile.data.title = vFile.data.title || vFile.data.name;
 
-      const values = { data: vFile.data, content: vFile.toString() };
+      const html = vFile.toString();
+      const { data } = vFile;
+      const content = await liquidProcessor.parseAndRender(html, data);
 
-      const { data } = await supabaseAdmin
+      const { data: resData } = await supabaseAdmin
         .from("test")
-        .insert(values)
+        .insert({ data, content })
         .select("id")
         .throwOnError()
         .single();
 
-      return res.status(200).json(data);
+      return res.status(200).json(resData);
     } catch (error) {
       return res.status(500).end(error);
     }
